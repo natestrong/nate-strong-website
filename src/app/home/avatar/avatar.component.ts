637eb9ed99e9a1973b2a909b5e59ee39e91e079c
nstrong <nstrong@spanimation.com>
2021-06-24 10:20:08 -0700

    changes some styling

--- src/app/home/avatar/avatar.component.ts
+++ src/app/home/avatar/avatar.component.ts
@@ -3,12 +3,23 @@ import {Component, OnInit, ChangeDetectionStrategy} from '@angular/core';
 @Component({
   selector: 'app-avatar',
   template: `
-    <section fxLayout="row" fxLayoutGap="12px" fxLayoutAlign="start center" >
+    <section fxLayout="row" fxLayoutGap="12px" fxLayoutAlign="start center">
       <img src="assets/me.jpeg" alt="a picture of me">
-      <p>
+      <p class="description">
         Hello, friends!
-        <br>My name is Nathan Strong and I am a software engineer.
-        <br>I work at Sony Pictures Animation and this is my personal blog.
+        <span>My name is Nathan Strong and I am a software engineer.</span>
+        <span *ngIf="descriptionLevel > 0">I work at Sony Pictures Animation and this is my personal blog.</span>
+        <span *ngIf="descriptionLevel > 1">I am a full stack engineer, and I am especially fond of Node, Python, and Angular.</span>
+        <span *ngIf="descriptionLevel > 2"><br>I am currently fascinated by these topics:
+          <br>&nbsp; • New Browser and JavaScript APIs
+          <br>&nbsp; • Serverless
+          <br>&nbsp; • Node.js
+          <br>&nbsp; • Open Source
+          <br>&nbsp; • Developer Tooling
+          <br>&nbsp; • JavaScript build tools
+          <br>&nbsp; • Compilers
+      </span>
+      <a *ngIf="descriptionLevel < 3" (click)="onClickMore()">...</a>
       </p>
     </section>
   `,
@@ -16,6 +27,7 @@ import {Component, OnInit, ChangeDetectionStrategy} from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AvatarComponent implements OnInit {
+  descriptionLevel = 0;
 
   constructor() {
   }
@@ -23,4 +35,7 @@ export class AvatarComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  onClickMore() {
+    this.descriptionLevel++;
+  }
 }
